b99b055e100da20246cbcb578a210dbcbb0bd702
Cristopher Araujo <cristopher.araujo1@gmail.com>
2023-01-14 09:19:34 -0300

    Incio do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent} from './src/views/form/form.component'
+import { LoginComponent} from './src/views/login/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'form',component:FormComponent
+},
+{path:'login',component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './src/views/form/form.component';
+import { LoginComponent } from './src/views/login/login.component';
+import { Form2Component } from './src/views/form2/form2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    LoginComponent,
+    Form2Component
   ],
   imports: [
     BrowserModule,
